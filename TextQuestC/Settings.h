8880964c37ec44d14a46631697c9fe1057dfa6b6
AMBULATUR <ambulatur@gmail.com>
2018-02-20 10:48:52 +0300

    Resolution Define

--- TextQuestC/Settings.h
+++ TextQuestC/Settings.h
@@ -1,13 +1,14 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
-
+#define ResolutionX 1920/2
+#define ResolutionY 1080/2
 void Settings(sf::RenderWindow & window)
 {
 	Texture settingsBackground;
 	settingsBackground.loadFromFile("images/settings.png");
 	Sprite background(settingsBackground);
 	background.setPosition(0, 0);
-	window.create(VideoMode(1920, 1080, 32), "TextQuest", sf::Style::None);
+	window.create(VideoMode(ResolutionX, ResolutionY, 32), "TextQuest", sf::Style::None);
 	window.draw(background);
 	window.display();
 	while (!Keyboard::isKeyPressed(Keyboard::Escape));
